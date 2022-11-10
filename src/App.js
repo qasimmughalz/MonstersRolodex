58bf11be3253b2512e324fa20b444e8a245eeae7
Muhammad Qasim <imrqasim@gmail.com>
2022-11-10 14:12:59 +0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+import {Component} from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      Monsters: [], 
+      searchField : ''
+    }
+  }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then((res)=>  res.json())
+    .then((users)=> {
+      this.setState( {Monsters : users} )
+    })
+    .catch((err)=> console.log(err))
+  }
+
+  render(){
+
+    const filtered = this.state.Monsters.filter((monster) => monster.name.toLowerCase().includes(this.state.searchField)) 
+    return (
+        
+      <div className="App">
+        <input placeholder='Search Monster' 
+          onChange={(eve) =>  {
+          this.setState( ()=> {
+            return {
+                searchField: eve.target.value
+            }
+          }) }
+        }  
+           />
+
+        {filtered.map((users =>{
+          return <h1 key={users.id}>{users.name}</h1>
+        }))}
+      </div>
+    );
+  }
+
 }
 
 export default App;
